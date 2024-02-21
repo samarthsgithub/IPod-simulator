d5fee3254db5cfb4f529e12fda45b8d8ce50ebec
samarthsgithub <123881350+samarthsgithub@users.noreply.github.com>
2024-02-22 00:51:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./index.css"
+import Screen from "./screen.js"
+import Circular from "./circular.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  
+ constructor(){
+  super();
+  this.state = {
+    activeIndex:0, // to check which menu item needs to be blue or selected
+    menuItems:["Cover Flow","Music","Games","Settings"], // state items to be rendered on menu 
+    showMenuItems:true,  // to check if menu items need to be shown or not
+    currentMenu:0 // index for checking currentMenu to be rendered on screen
+  };
+ }
+ //function to update activeMenuIndex on rotating wheel
+  updateActiveIndex = (direction)=>{
+    let{activeIndex} = this.state;
+    if(direction==="clockwise"){
+      activeIndex = (activeIndex+1) % 4;
+    }else if(direction==="counterclockwise"){
+       activeIndex = (activeIndex-1 +4)%4;
+    }
+    this.setState({
+      activeIndex
+    })
+  }
+  // function to handle the centre click button
+  handleCentreClick = ()=>{
+    let{currentMenu,activeIndex,showMenuItems} = this.state;
+       if(activeIndex==0){
+        currentMenu = 1;
+       }else if(activeIndex==1){
+         currentMenu = 2;
+       }else if(activeIndex==2){
+        currentMenu = 3;
+       }else if(activeIndex==3){
+        currentMenu = 4;
+       }
+       showMenuItems=!showMenuItems
+       this.setState({
+           currentMenu,
+           showMenuItems
+       })
+       console.log(showMenuItems)
+       
+  }
+  // to handle the menu button for going back to menu
+  handleMenuButton= ()=>{
+          let{currentMenu,showMenuItems}  =this.state;
+          currentMenu = 0;
+          showMenuItems= true;
+          this.setState({
+            currentMenu,
+            showMenuItems
+          })
+          console.log(showMenuItems);
+  }
+  
+
+  render(){
+   let{x} = this.state;
+    return(
+      <>
+        <h1>IPOD APP</h1>
+        <p>Hello How are you</p>
+        <div className="IPOD-Container">
+         
+           <Screen menuItems={this.state.menuItems} activeIndex={this.state.activeIndex}
+                   showMenuItems={this.state.showMenuItems} onclickshowitems={this.state.onclickshowitems}
+                   currentMenu={this.state.currentMenu}
+           ></Screen>
+           <Circular updateActiveIndex = {this.updateActiveIndex}
+                     handleCentreClick={this.handleCentreClick}
+                     handleMenuButton={this.handleMenuButton}
+           ></Circular>
+        </div>
+      </>
+    )
+  }
+
 }
 
 export default App;
