6abb6e24db07cd676c6ff46f32e5fd985a70df3b
samarthsgithub <123881350+samarthsgithub@users.noreply.github.com>
2024-02-22 01:57:21 +0530

    Update games.js

--- src/games.js
+++ src/games.js
@@ -7,7 +7,7 @@ class Games extends React.Component{
     render(){
         return(
             <>
-            <img src="https://w0.peakpx.com/wallpaper/876/783/HD-wallpaper-dice-cards-casino-dark-gambling-indoor-games-ludo-monopoly-poker-red.jpg" style={{height:"15rem", width:"24rem" }}></img>
+            <img src="https://w0.peakpx.com/wallpaper/876/783/HD-wallpaper-dice-cards-casino-dark-gambling-indoor-games-ludo-monopoly-poker-red.jpg" style={{height:"15rem", width:"24rem" }} alt=""></img>
             </>
         )
     }
